0b25570e5d0fcce672dcf38e4543daea11c6ea92
Luis Mora <luisma1985@gmail.com>
2023-03-25 09:41:51 -0500

    Create functional guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { inject, NgModule } from '@angular/core';
+import { Router, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { AuthService } from './core/auth.service';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  {
+    path: '',
+    component: HomeComponent,
+    canActivate: [
+      () => {
+        const authService = inject(AuthService);
+        const router = inject(Router);
+        if (authService.isLogged()) {
+          return true;
+        } else {
+          router.navigate(['/login']);
+          return false;
+        }
+      },
+    ],
+  },
   { path: 'login', component: LoginComponent },
 ];
 
