bea4b1d7babf33d62f08f74b3741060428d10556
Luis Mora <luisma1985@gmail.com>
2023-03-26 19:51:08 -0500

    Add dependencies to tests

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,23 +1,40 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
+import { AuthService } from 'src/app/core/auth.service';
+import { Router } from '@angular/router';
+import { InfoComponent } from '../info/info.component';
+import { FormsModule } from '@angular/forms';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let authService: AuthService;
+  let router: Router;
 
   beforeEach(async () => {
+    const authServiceSpy = jasmine.createSpyObj<AuthService>('AuthService', [
+      'logout',
+      'isLogged',
+    ]);
+    const routerSpy = jasmine.createSpyObj<Router>('Router', ['navigateByUrl']);
+
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
+      declarations: [HomeComponent, InfoComponent],
+      providers: [
+        { provide: AuthService, useValue: authServiceSpy },
+        { provide: Router, useValue: routerSpy },
+      ],
+      imports: [FormsModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
+    authService = TestBed.inject(AuthService);
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
   });
 });
