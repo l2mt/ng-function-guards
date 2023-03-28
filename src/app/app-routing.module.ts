d854fe07e78b8b3f57133ac894c51ae97f3f4656
Luis Mora <luisma1985@gmail.com>
2023-03-27 22:31:36 -0500

    Add functional guard and lazy load component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,34 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { inject, NgModule } from '@angular/core';
+import { Router, RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './components/admin/admin.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { AuthService } from './core/auth.service';
+
+const authGuardFn = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  if (authService.isLogged()) {
+    return true;
+  }
+  router.navigateByUrl('/login');
+  return false;
+};
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  {
+    path: '',
+    component: HomeComponent,
+  },
   { path: 'login', component: LoginComponent },
+  {
+    path: 'admin',
+    canActivate: [authGuardFn],
+    loadComponent: () =>
+      import('./components/admin/admin.component').then(
+        (m) => m.AdminComponent
+      ),
+  },
 ];
 
 @NgModule({
---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../core/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    console.log('AuthGuard Route', route);
+    console.log('AuthGuard State', state);
+
+    if (this.authService.isLogged()) {
+      return true;
+    }
+
+    this.router.navigateByUrl('/login');
+    return false;
+  }
+}
